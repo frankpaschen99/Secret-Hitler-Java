04fc579a7a49b244f88c6cb56a2c9d334d9993b4
Frank Paschen <frankpaschen99@gmail.com>
2017-01-08 09:35:15 -0500

    Implemented Screens interface

---
+++ core/src/com/codelackeys/screens/WelcomeScreen.java
@@ -0,0 +1,49 @@
+package com.codelackeys.screens;
+
+import com.badlogic.gdx.Screen;
+
+public class WelcomeScreen implements Screen {
+
+	/* The Welcome screen will include two buttons: Create Lobby and Join Lobby. */
+	
+	@Override
+	public void show() {
+		// This is called once, when setScreen() is called
+	}
+
+	@Override
+	public void render(float delta) {
+		// called every iteration of the game loop
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		// Probably not going to allow this
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
