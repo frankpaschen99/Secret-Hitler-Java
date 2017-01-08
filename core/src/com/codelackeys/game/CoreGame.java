562a8d39dea75090e0580aa3b6f5767e555ad42a
Frank Paschen <frankpaschen99@gmail.com>
2017-01-08 09:36:47 -0500

    rendering

--- core/src/com/codelackeys/game/CoreGame.java
+++ core/src/com/codelackeys/game/CoreGame.java
@@ -12,11 +12,6 @@ public class CoreGame extends Game {
 
 	@Override
 	public void render () {
-		/*Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		
-		batch.end();*/
 		super.render();
 	}
 }
--- core/src/com/codelackeys/screens/WelcomeScreen.java
+++ core/src/com/codelackeys/screens/WelcomeScreen.java
@@ -1,19 +1,36 @@
 package com.codelackeys.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class WelcomeScreen implements Screen {
 
 	/* The Welcome screen will include two buttons: Create Lobby and Join Lobby. */
 	
+	private SpriteBatch batch;
+	
 	@Override
 	public void show() {
 		// This is called once, when setScreen() is called
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render(float delta) {
 		// called every iteration of the game loop
+		// used for drawing and handling input
+		
+		// Must clear the buffer every iteration or we get that weird windows xp bug
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		
+		// draw welcome screen here
+		
+		batch.end();
 	}
 
 	@Override
